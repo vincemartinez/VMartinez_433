cfdadc30ae403d4c15c6b9b7d8708ae18dce8010
vincemartinez <vincentmartinez2015@u.northwestern.edu>
2015-04-28 15:09:37 -0500

    debug

--- HW8/hidapi-master/hidtest/hidtest.cpp
+++ HW8/hidapi-master/hidtest/hidtest.cpp
@@ -66,8 +66,8 @@ int main(int argc, char* argv[])
 	scanf("%[^\n]s", message);
 
 	printf("Input the row for the string to be displayed:\n");
-	char row;
-	scanf("%c", &row);
+	int row=0;
+	scanf("%x", &row);
 
 	buf[0] = 0x0;
 	printf("im here\n");
